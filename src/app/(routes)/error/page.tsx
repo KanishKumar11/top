47e2a729af0574ec4fa4f07b1a18bd26a6e9a89e
KanishKumar11 <kanishkumar11@outlook.in>
2024-03-03 21:00:45 +0530

    trying to resolve the error

--- src/app/(routes)/error/page.tsx
+++ src/app/(routes)/error/page.tsx
@@ -1,46 +1,7 @@
-"use server";
+import React from "react";
 
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+const page = () => {
+  return <div>error</div>;
+};
 
-import { createClient } from "@/utils/supabase/server";
-
-export async function login(formData: FormData) {
-  const supabase = createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
-
-  const { error } = await supabase.auth.signInWithPassword(data);
-
-  if (error) {
-    redirect("/error");
-  }
-
-  revalidatePath("/", "layout");
-  redirect("/");
-}
-
-export async function signup(formData: FormData) {
-  const supabase = createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
-
-  const { error } = await supabase.auth.signUp(data);
-
-  if (error) {
-    redirect("/error");
-  }
-
-  revalidatePath("/", "layout");
-  redirect("/");
-}
+export default page;
